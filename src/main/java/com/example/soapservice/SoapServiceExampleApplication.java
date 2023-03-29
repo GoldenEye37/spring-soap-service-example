58ce18b7212682e2f051294d221aa43a3f9dac4d
LDhimba <Ldhimba@bancabc.co.zw>
2023-03-29 10:10:20 +0200

    soap web service example

---
+++ src/main/java/com/example/soapservice/SoapServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapServiceExampleApplication.class, args);
+    }
+
+}
